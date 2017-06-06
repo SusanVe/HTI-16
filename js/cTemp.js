00b999587c90868f10cb597003966c90cdfe49c3
TRJM2207 <tommulders1998@gmail.com>
2017-06-06 11:46:30 +0200

    increment fix

--- js/cTemp.js
+++ js/cTemp.js
@@ -6,18 +6,18 @@ function changeTemp(sliderID, textbox) {
 
 function minTemp() {
     var t = document.getElementById("slider").value;
-    if (t > 10) {
-        t=t-0.5;   
+    if (t > 5) {
+        t= Math.round(10*(t-0.1))/10;
         document.getElementById("slider").value=t;
-        document.getElementById("cTemp").innerHTML=t+" C&deg;";           
-    } 
+        document.getElementById("cTemp").innerHTML=t+" C&deg;";
+    }
 }
 
 function plusTemp() {
     var t = document.getElementById("slider").value;
     if (t < 30) {
-        t=t-(-0.5);   
+        t= Math.round( 10 *(t-(-0.1)))/10;
         document.getElementById("slider").value=t;
-        document.getElementById("cTemp").innerHTML=t+" C&deg;";           
-    } 
+        document.getElementById("cTemp").innerHTML=t+" C&deg;";
+    }
 }

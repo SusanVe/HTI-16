f791812a32b5781b52bff3f1a6e4513a23fc6140
SusanVe <susanvew@hotmail.com>
2017-06-15 11:02:31 +0200

    increase/decrease fix

--- js/cTemp.js
+++ js/cTemp.js
@@ -1,25 +1,16 @@
 var ServerUrl = 'http://wwwis.win.tue.nl/2id40-ws/16';
-
-function changeTemp(sliderID, textbox) {
-    var x = document.getElementById(textbox);
-    var y = document.getElementById(sliderID);
-    x.innerHTML = y.value+" C&deg;";
-}
+var t=21;
 
 function minTemp() {
-    var t = document.getElementById("slider").value;
     if (t > 5) {
         t= Math.round(10*(t-0.1))/10;
-        document.getElementById("slider").value=t;
         document.getElementById("cTemp").innerHTML=t+" C&deg;";
     }
 }
 
 function plusTemp() {
-    var t = document.getElementById("slider").value;
     if (t < 30) {
         t= Math.round( 10 *(t-(-0.1)))/10;
-        document.getElementById("slider").value=t;
         document.getElementById("cTemp").innerHTML=t+" C&deg;";
     }
 }

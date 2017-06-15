a5ad7cacf584d6cebf015fdd64dca0d98f71a94e
Joostdeboer <joostdeboer97@gmail.com>
2017-06-15 10:37:41 +0200

    daytemp nighttemp to server

--- js/daynight.js
+++ js/daynight.js
@@ -1,7 +1,34 @@
-function submitDay(){
+var ServerUrl = 'http://wwwis.win.tue.nl/2id40-ws/16';
+
+function submitDay(attribute_name, xml_tag, value){
   var dayTemp = document.getElementById("dayTemp").value;
+  put("dayTemperature", "day_temperature", dayTemp);
 }
 
-function submitNight(){
+function submitNight(attribute_name, xml_tag, value){
   var nightTemp = document.getElementById("nightTemp").value;
+  put("nightTemperature", "night_temperature", nightTemp);
+}
+
+function get(attribute_name, xml_tag) {
+    return requestData(
+        "/"+attribute_name,
+        function(data) {
+            return $(data).find(xml_tag).text();
+        }
+    );
+}
+
+function put(attribute_name, xml_tag, value){
+    uploadData("/"+attribute_name, "<" + xml_tag + ">"+ value + "</" + xml_tag + ">");
+}
+
+function uploadData(address, xml) {
+    $.ajax({
+        type: "put",
+        url: ServerUrl + address,
+        contentType: 'application/xml',
+        data: xml,
+        async: false
+    });
 }

310e44dd1fbeaec8bf6c3d6a10d20f2c7fd1cabf
TRJM2207 <tommulders1998@gmail.com>
2017-06-15 12:17:51 +0200

    display server temp & merged ctemp & gettemp

--- js/cTemp.js
+++ js/cTemp.js
@@ -1,7 +1,8 @@
 var ServerUrl = 'http://wwwis.win.tue.nl/2id40-ws/16';
-var t=21;
+var t= 21;
 
 function minTemp() {
+  var t = getTemp();
     if (t > 5) {
         t= Math.round(10*(t-0.1))/10;
         document.getElementById("cTemp").innerHTML=t+" °C";
@@ -34,4 +35,31 @@ function put(attribute_name, xml_tag, value){
     function update(attribute_name, xml_tag, value) {
       put("targetTemperature", "target_temperature", "12.0");
 
+    }
+    function getTemp() {
+      function requestData(address, func) {
+          var result;
+          $.ajax({
+              type: "get",
+              url: ServerUrl + address,
+              dataType: "xml",
+              async: false,
+              success: function(data) {
+                  result = func(data);
+              }
+          });
+          return result;
+      }
+
+
+      function get(attribute_name, xml_tag) {
+          return requestData(
+              "/"+attribute_name,
+              function(data) {
+                  return $(data).find(xml_tag).text();
+              }
+          );
+      }
+      var temp = get("currentTemperature", "current_temperature");
+      document.getElementById("cTemp").innerHTML= temp;
     }
